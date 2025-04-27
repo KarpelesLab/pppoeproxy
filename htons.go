93f76493adc2291550e90a8acafdd9027c0a62cb
Mark Karpeles <magicaltux@gmail.com>
2025-04-27 11:26:20 +0900

    Simplify byte order conversion functions
    - Refactor htons to use direct bit shifting for more efficient byte swapping
    - Eliminate unnecessary slice allocations for better performance
    - Simplify ntohs implementation to reuse htons logic
    - Maintain endian-aware implementation that only swaps bytes when needed

--- htons.go
+++ htons.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"encoding/binary"
-	"unsafe"
 )
 
 // isLittleEndian checks whether the machine is little endian
 func isLittleEndian() bool {
-	var i int32 = 0x01020304
-	// Convert to byte slice to check the first byte
-	u := unsafe.Pointer(&i)
-	pb := (*byte)(u)
-	return *pb == 0x04
+	// Create a byte slice with known values
+	buf := []byte{0x12, 0x34}
+
+	// If machine is little endian, the value will be interpreted as 0x3412
+	// If machine is big endian, the value will be interpreted as 0x1234
+	return binary.LittleEndian.Uint16(buf) == uint16(0x3412)
 }
 
 // littleEndianMachine is a global variable indicating whether the machine is little endian
@@ -21,16 +21,14 @@ var littleEndianMachine = isLittleEndian()
 func htons(i uint16) uint16 {
 	// Only swap if the machine is little endian
 	if littleEndianMachine {
-		return binary.BigEndian.Uint16(binary.LittleEndian.AppendUint16(nil, i))
+		// Simple byte swap using bit shifting
+		return (i << 8) | (i >> 8)
 	}
 	return i
 }
 
 // ntohs converts a short (uint16) from network byte order (big endian) to host byte order
 func ntohs(i uint16) uint16 {
-	// Only swap if the machine is little endian
-	if littleEndianMachine {
-		return binary.LittleEndian.Uint16(binary.BigEndian.AppendUint16(nil, i))
-	}
-	return i
+	// Network to host is the same operation as host to network
+	return htons(i)
 }
